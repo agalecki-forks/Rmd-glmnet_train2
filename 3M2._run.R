789490e14fb973c7bb6814422da21409f8e58ae5
agalecki <agalecki@umich.edu>
2022-12-19 19:01:05 -0500

    varia

--- 3M2._run.R
+++
@@ -1,7 +0,0 @@
- # Clears Global environment
- rm(list=ls())
-
- bnm <-"3M2_Cox"   # Basename
- source("00init.txt")
- 
- 
--- 4._run_M2a.R
+++ 4._run_M2a.R
@@ -1,7 +1,7 @@
  # Clears Global environment
  rm(list=ls())
 
- bnm <-"3.Cox_Reg_2a_model"   # Basename
+ bnm <-"4.Cox_Reg_2a_model"   # Basename
  source("00init.txt")
  
  
